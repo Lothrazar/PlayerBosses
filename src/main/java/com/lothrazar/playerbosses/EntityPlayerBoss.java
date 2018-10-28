70fbc30a41a9bd5b6f41e906c177e248fffbf8bb
sam <samson.bassett@gmail.com>
2018-10-28 12:28:01 -0700

    it attacks now

--- src/main/java/com/lothrazar/playerbosses/EntityPlayerBoss.java
+++ src/main/java/com/lothrazar/playerbosses/EntityPlayerBoss.java
@@ -2,11 +2,19 @@ package com.lothrazar.playerbosses;
 
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityGiantZombie;
+import net.minecraft.entity.monster.EntityPigZombie;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
@@ -16,11 +24,13 @@ public class EntityPlayerBoss extends EntityGiantZombie {
 
   public EntityPlayerBoss(World worldIn) {
     super(worldIn);
+    EntityZombie cloneme;
   }
 
   @Override
   protected void applyEntityAttributes() {
     super.applyEntityAttributes();
+    this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(2.0D);
     //  this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.20D);
   }
 
@@ -33,10 +43,21 @@ public class EntityPlayerBoss extends EntityGiantZombie {
   @Override
   protected void initEntityAI() {
     super.initEntityAI();
-    this.tasks.addTask(1, new EntityAISwimming(this));
-    this.tasks.addTask(6, new EntityAILookIdle(this));
-    this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+    this.tasks.addTask(0, new EntityAISwimming(this));
+    this.tasks.addTask(2, new EntityAIAttackMelee(this, 1.0D, false));
+    this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+    this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
+    this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+    this.tasks.addTask(8, new EntityAILookIdle(this));
+    this.applyEntityAI();
+  }
+
+  protected void applyEntityAI() {
+    this.tasks.addTask(6, new EntityAIMoveThroughVillage(this, 1.0D, false));
+    this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true, new Class[] { EntityPigZombie.class }));
     this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, true));
+    //    this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityVillager.class, false));
+    //    this.targetTasks.addTask(3, new EntityAINearestAttackableTarget(this, EntityIronGolem.class, true));
   }
 
   @Override
